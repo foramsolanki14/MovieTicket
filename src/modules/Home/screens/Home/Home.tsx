a7be623b1d8aa68fb3b645076d216460b9b5967e
foram <foramsolanki987@gmail.com>
2025-03-07 15:32:42 +0530

    edit home

--- src/modules/Home/screens/Home/Home.tsx
+++ src/modules/Home/screens/Home/Home.tsx
@@ -1,10 +1,11 @@
-import {View, Text} from 'react-native';
+import {View, Text, Image} from 'react-native';
 import React from 'react';
 
 const Home = () => {
   return (
     <View>
       <Text>Home</Text>
+      <Image source={require('../../../../assets/tab/home.png')} />
     </View>
   );
 };
