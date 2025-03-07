c6bda580bafc368c263685cc91ff3ed83fa6bf78
foram <foramsolanki987@gmail.com>
2025-03-07 15:33:07 +0530

    add image extantion

---
+++ global.d.ts
@@ -0,0 +1,20 @@
+declare module '*.svg' {
+  import React from 'react';
+  import {SvgProps} from 'react-native-svg';
+  const content: React.FC<SvgProps>;
+  export default content;
+}
+
+declare module '*.png' {
+  const value: ImageSourcePropType;
+  export default value;
+}
+declare module '*.webp' {
+  const value: ImageSourcePropType;
+  export default value;
+}
+
+declare module '*.lottie' {
+  const value: ImageSourcePropType;
+  export default value;
+}
