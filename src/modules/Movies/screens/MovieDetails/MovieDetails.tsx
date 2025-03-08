52d206789668da11ee5baad55cd8dc5e8758eed1
foram <foramsolanki987@gmail.com>
2025-03-08 17:25:00 +0530

    add image and navigate

--- src/modules/Movies/screens/MovieDetails/MovieDetails.tsx
+++ src/modules/Movies/screens/MovieDetails/MovieDetails.tsx
@@ -1,12 +1,59 @@
-import {View, Text} from 'react-native';
+import {
+  View,
+  Text,
+  Image,
+  StyleSheet,
+  TouchableOpacity,
+  Button,
+} from 'react-native';
 import React from 'react';
 
-const MovieDetails = () => {
+function MovieDetails({route}) {
+  const {movie} = route.params;
   return (
-    <View>
-      <Text>MovieDetails</Text>
+    <View style={styles.Conatiner}>
+      <Text style={styles.moviename}>{movie.title}</Text>
+      <Image source={movie.image} style={styles.img} />
+      <Text style={styles.Details}>{movie.Details}</Text>
+
+      <TouchableOpacity style={styles.btn}>
+        <Text style={styles.btntxt}>Book Tickets</Text>
+      </TouchableOpacity>
     </View>
   );
-};
+}
 
 export default MovieDetails;
+
+const styles = StyleSheet.create({
+  Conatiner: {
+    flex: 1,
+    padding: 10,
+  },
+  moviename: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  img: {
+    height: 260,
+    width: 390,
+    borderRadius: 10,
+  },
+  Details: {
+    marginTop: 20,
+    fontSize: 15,
+  },
+
+  btn: {
+    backgroundColor: '#e33653',
+    height: 30,
+    marginTop: 370,
+    justifyContent: 'center',
+  },
+  btntxt: {
+    textAlign: 'center',
+    color: '#f5f0f1',
+    fontWeight: 'bold',
+  },
+});
--- src/modules/Movies/screens/Movies/Movies.tsx
+++ src/modules/Movies/screens/Movies/Movies.tsx
@@ -10,31 +10,43 @@ const Movies = () => {
       id: 1,
       title: 'Chhaava',
       image: require('../../../../assets/Chhaava.webp'),
+      Details:
+        'Chhaava is a 2025 Indian Hindi-language historical action film based on the life of Sambhaji Maharaj, the second ruler of the Maratha Empire, who is played ...',
     },
     {
       id: 2,
       title: 'Fati Ne ?',
       image: require('../../../../assets/Fatine.png'),
+      Details:
+        'Param Laal and Padam Laal, two dimwitted but well-meaning cops in Melbourne, owe their jobs to their legendary uncle Velji',
     },
     {
       id: 3,
       title: 'Umbaro',
       image: require('../../../../assets/Umbaro.webp'),
+      Details:
+        'Seven women from rural Gujarat embark on their first international trip to London, facing cultural clashes, language barriers in word.',
     },
     {
       id: 4,
       title: 'Mom Tne Nai Samjay',
       image: require('../../../../assets/MomTneNaiSmjai.webp'),
+      Details:
+        'Set in London, this emotional drama follows the life of Aashka and Kunal, a couple who strive to uphold Indian culture while raising their children',
     },
     {
       id: 5,
       title: 'The Monkey',
       image: require('../../../../assets/TheMonkey.webp'),
+      Details:
+        'It follows twin brothers whose lives are turned upside down by a cursed toy monkey that causes random horrific deaths around them.',
     },
     {
       id: 6,
       title: 'Best Of Luck Pandya',
       image: require('../../../../assets/BestOfLuckPandya.png'),
+      Details:
+        ' Comedy Drama released in Gujarati language in theatre near you in kolkata. Know about Film reviews, lead cast & crew, ...',
     },
   ];
 
@@ -42,9 +54,11 @@ const Movies = () => {
     <FlatList
       numColumns={2}
       data={Movies}
+      keyExtractor={item => item.id}
       renderItem={({item}) => (
         <View style={styles.main}>
-          <Pressable onPress={() => navigation.navigate('MovieDetails')}>
+          <Pressable
+            onPress={() => navigation.navigate('MovieDetails', {movie: item})}>
             <Image source={item.image} style={styles.img} />
           </Pressable>
 
