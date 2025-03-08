0a0fbdbee16a24ec6d3500cf24088cb47bac1415
foram <foramsolanki987@gmail.com>
2025-03-08 17:38:16 +0530

    add navigation Cinema

--- src/navigation/NavigationBuilder.tsx
+++ src/navigation/NavigationBuilder.tsx
@@ -8,6 +8,7 @@ import MyProfile from '../modules/Profile/screens/MyProfile/MyProfile';
 import YourOrders from '../modules/Profile/screens/YourOrders/YourOrders';
 import MovieDetails from '../modules/Movies/screens/MovieDetails/MovieDetails';
 import SelectSheat from '../modules/Movies/screens/SelectSheat/SelectSheat';
+import Cinema from '../modules/Movies/screens/Cinema/Cinema';
 
 const Stack = createNativeStackNavigator();
 
@@ -22,6 +23,7 @@ const AppStack = () => {
       </Stack.Group>
       <Stack.Group screenOptions={{headerShown: true}}>
         <Stack.Screen name={ScreenName.MovieDetails} component={MovieDetails} />
+        <Stack.Screen name={ScreenName.Cinema} component={Cinema} />
         <Stack.Screen name={ScreenName.SelectSheat} component={SelectSheat} />
       </Stack.Group>
     </Stack.Navigator>
