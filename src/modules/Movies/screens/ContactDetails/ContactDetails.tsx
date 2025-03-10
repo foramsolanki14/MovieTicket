8883a7d4d8d17825b2b2a30a134a90a506fbb41d
foram <foramsolanki987@gmail.com>
2025-03-10 18:57:58 +0530

    Add folder with file

---
+++ src/modules/Movies/screens/ContactDetails/ContactDetails.tsx
@@ -0,0 +1,12 @@
+import {View, Text} from 'react-native';
+import React from 'react';
+
+const ContactDetails = () => {
+  return (
+    <View>
+      <Text>ContactDetails</Text>
+    </View>
+  );
+};
+
+export default ContactDetails;
